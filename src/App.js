4114ae9c490a10a39e5cc80738adb4e9c1595ad5
Keaton Singer <keatonsinger@Keatons-MacBook-Pro-2.local>
2023-01-09 15:14:57 -0800

    Working on BFS

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 function App() {
   const speed = useRef(50);
   const stackDFS = useRef([]);
+  const queueBFS = useRef([]);
   const [bitmap, setBitmap] = useState([]);
-  const [algorithm, setAlogirthm] = useState("DFS");
+  const [algorithm, setAlgorithm] = useState("DFS");
   const [algOpt, setAlgOpt] = useState("none");
   const [speedOpt, setSpeedOpt] = useState("none");
   const [toolsOpt, setToolsOpt] = useState("none");
   const [gridOpt, setGridOpt] = useState("none");
-  const [startStop, setStartStop] = useState(["Start!", "green"]);
-  const startP = useRef("Start!");
+  const [startButton, setStartButton] = useState("Start!");
+  const startPersist = useRef("Start!");
   const [origin, setOrigin] = useState(288);
   const [target, setTarget] = useState(800);
   const [walls, setWalls] = useState([]);
@@ -24,17 +25,12 @@ function App() {
     GenerateBitmap();
   }, [origin, target, walls, path.current, pathHead, tool]);
   useEffect(() => {
-    // if (startStop[0] === "Stop!" && algorithm === "BFS") {
-    //   if (path.current.length > 0) BFS(path.current[path.current.length - 1]);
-    //   else BFS(origin);
-    // };
-    if (startStop[0] === "Stop!" && algorithm === "DFS") {
-      if (path.current.length > 0) DFS(path.current[path.current.length - 1]);
-      else DFS(origin);
-    };
-  }, [startStop]);
-  
-  
+    if (startButton !== "Stop!") { return; }
+    else if (algorithm === "DFS" && path.current.length === 0) { DFS(origin); }
+    else if (algorithm === "DFS") { DFS(path.current[path.current.length - 1]); }
+    else if (algorithm === "BFS" && path.current.length === 0) { BFS(origin); }
+    else if (algorithm === "BFS") { BFS(path.current[path.current.length - 1]); }
+  }, [startButton]);
   window.addEventListener('mousedown', () => {
     mouseHeld.current = true;
   });
@@ -50,7 +46,7 @@ function App() {
 
   function Draw(bitIndex, action) {
     if (action === "drag" && mouseHeld.current === false) { return; }
-    if (startStop[0] !== "Start!" || tool === "") { return; }
+    if (startButton !== "Start!" || tool === "") { return; }
     else if (tool === "Move Origin" && path.current.length !== 0) { return; }
     else if (tool === "Move Origin" && bitIndex !== target && !walls.includes(bitIndex) && path.current.length === 0) { setOrigin(bitIndex); }
     else if (tool === "Move Target" && bitIndex !== origin && !walls.includes(bitIndex) && !path.current.includes(bitIndex)) { setTarget(bitIndex); }
@@ -70,26 +66,53 @@ function App() {
   };
 
 
-  async function DFS(pixel) {
+  async function DFS(currentBit) {
     function stackPush(item) { stackDFS.current = [...stackDFS.current, item]; };
-    if (stackDFS.current.length === 0) { stackPush(pixel); };
+    if (stackDFS.current.length === 0) { stackPush(currentBit); };
     let topItem = -1;
-    while (stackDFS.current.length && startP.current === "Stop!") {
+    while (stackDFS.current.length && startPersist.current === "Stop!") {
       await sleep(speed.current);
       let lastIndex = stackDFS.current.length - 1;
       topItem = stackDFS.current[lastIndex];
       stackDFS.current = stackDFS.current.slice(0, lastIndex);
-      if (topItem === target) { setPathHead(topItem); break; }
       if (walls.includes(topItem)) { continue; }
       setPathHead(topItem);
+      if (topItem === target) { break; }
       path.current = [...path.current, topItem];
       if (topItem % 33 > 0 && EmptyBit(topItem - 1, path.current)) { stackPush(topItem - 1); };
       if (topItem < 1056 && EmptyBit(topItem + 33, path.current)) { stackPush(topItem + 33); };
       if (topItem % 33 < 32 && EmptyBit(topItem + 1, path.current)) { stackPush(topItem + 1); };
       if (topItem > 32 && EmptyBit(topItem - 33, path.current)) { stackPush(topItem - 33); };
     };
-    if (topItem === target || stackDFS.current.length === 0) { setStartStop(["Clear Path!", "grey"]); }
+    if (path.current.indexOf(undefined) > -1 || path.current.length === 0) { path.current = []; return; }
+    else if (topItem === target || stackDFS.current.length === 0) { setStartButton("Clear Path!"); };
   };
+
+
+  async function BFS(currentBit) {
+    function queueAppend(item) { queueBFS.current = [...queueBFS.current, item]; };
+    if (queueBFS.current.length === 0) { queueAppend(currentBit); };
+    let nextItem = -1;
+    while (queueBFS.length !== 0 && startPersist.current === "Stop!") {
+      let queueLength = queueBFS.current.length;
+      for (let queueIndex = 0; queueIndex < queueLength; queueIndex++) {
+        await sleep(speed.current)
+        let lastIndex = queueBFS.current.length - 1;
+        nextItem = queueBFS.current[0];
+        queueBFS.current = queueBFS.current.slice(1, lastIndex);
+        if (walls.includes(nextItem)) { continue; }
+        setPathHead(nextItem);
+        if (nextItem === target) { queueBFS.current = []; break; }
+        path.current = [...path.current, nextItem];
+        if (nextItem > 32 && EmptyBit(nextItem - 33) && !queueBFS.current.includes(nextItem - 33)) queueAppend(nextItem - 33);
+        if (nextItem % 33 > 0 && EmptyBit(nextItem - 1) && !queueBFS.current.includes(nextItem - 1)) queueAppend(nextItem - 1);
+        if (nextItem < 1056 && EmptyBit(nextItem + 33) && !queueBFS.current.includes(nextItem + 33)) queueAppend(nextItem + 33);
+        if (nextItem % 33 < 32 && EmptyBit(nextItem + 1) && !queueBFS.current.includes(nextItem + 1)) queueAppend(nextItem + 1);
+      }
+    }
+    if (path.current.indexOf(undefined) > -1 || path.current.length === 0) { path.current = []; return; }
+    else if (nextItem === target || queueBFS.current.length === 0) { setStartButton("Clear Path!"); };
+  }
 
 
   function GenerateBitmap() {
@@ -100,7 +123,9 @@ function App() {
             onMouseEnter={() => { Draw(bitIndex, "drag"); }} 
             onMouseDown={() => { Draw(bitIndex, "click"); }} 
             style={{backgroundColor: 
-              (bitIndex === pathHead) ? "green" : (bitIndex === origin || path.current.includes(bitIndex)) ? "rgb(120, 210, 130)" : (bitIndex === target) ? "rgb(210, 120, 130)" :
+              (bitIndex === pathHead) ? "green" : 
+              (bitIndex === origin || path.current.includes(bitIndex)) ? "rgb(120, 210, 130)" : 
+              (bitIndex === target) ? "rgb(210, 120, 130)" :
               (walls.includes(bitIndex) ? "rgb(40, 40, 40)" : "rgb(40, 100, 140)")
             }}>
           </div>
@@ -110,24 +135,27 @@ function App() {
   };
 
 
-  function CleanBitmap(extent) {
-    if (extent === "hard") {
+  function CleanBitmap(coverage) {
+    if (coverage === "full") {
       setOrigin(288);
       setTarget(800);
       setWalls([]);
     }
+    startPersist.current = "Start!"; 
+    setStartButton("Start!"); 
     stackDFS.current = [];
+    queueBFS.current = [];
     path.current = [];
     setPathHead(-1);
   }
 
 
-  function StartStop() {
-    if (startP.current === "Stop!" ) { startP.current = "Start!"; }
-    else { startP.current = "Stop!"; };
-    if (startStop[0] === "Start!") { setStartStop(["Stop!", "red"]); }
-    else if (startStop[0] === "Stop!") { setStartStop(["Start!", "green"]);}
-    else { CleanBitmap(); setStartStop(["Start!", "green"]); }
+  function ChangeButton() {
+    if (startPersist.current === "Stop!" ) { startPersist.current = "Start!"; }
+    else { startPersist.current = "Stop!"; };
+    if (startButton === "Start!") { setStartButton("Stop!"); }
+    else if (startButton === "Stop!") { setStartButton("Start!"); }
+    else { CleanBitmap("partial"); };
   };
 
 
@@ -148,6 +176,8 @@ function App() {
         <div id="Categories">
           <div className="Category" onClick={() => {ToggleCategory(algOpt, setAlgOpt)}}>
             Algorithms
+            <div onClick={() => { setAlgorithm("DFS"); CleanBitmap("partial"); }}>DFS</div>
+            <div onClick={() => { setAlgorithm("BFS"); CleanBitmap("partial"); }}>BFS</div>
           </div>
           <div className="Category" onClick={() => {ToggleCategory(speedOpt, setSpeedOpt)}}>
             Speed
@@ -164,13 +194,16 @@ function App() {
           </div>
           <div className="Category" onClick={() => {ToggleCategory(gridOpt, setGridOpt)}}>
             Bitmap
-            <div onClick={() => { CleanBitmap("hard"); }}>Reset Bitmap</div>
-            <div onClick={() => { CleanBitmap("soft"); }}>Clear Path</div>
+            <div onClick={() => { CleanBitmap("partial"); }}>Clear Path</div>
+            <div onClick={() => { CleanBitmap("full"); }}>Reset Bitmap</div>
           </div>
         </div>
       </div>
-      <div id="StartStop" onClick={() => {StartStop()}} style={{backgroundColor: startStop[1]}}>
-        {startStop[0]}
+      <div id="StartStop" 
+        onClick={() => { ChangeButton(); }} 
+        style={{backgroundColor: (startButton === "Start!") ? "green" : 
+                                 (startButton === "Stop!") ? "red" : "grey"}}>
+        {startButton}
       </div>
       <div id="Key"></div>
       <div id="BitmapContainer">
@@ -182,26 +215,3 @@ function App() {
 
 export default App;
 
-  // async function BFS(pixel) {
-  //   let queue = [pixel];
-  //   let pathCopy = path;
-  //   while (queue.length !== 0) {
-  //     let len = queue.length;
-  //     for (let index = 0; index < len; index++) {
-  //       await sleep(10)
-  //       let current = queue.shift();
-  //       if (current === dest) {
-  //         setCurr("");
-  //         return;
-  //       }
-  //       pathCopy.push(current);
-  //       setPath(path => [...path, current]);
-  //       setCurr(current);
-  //       if (current % 33 > 0 && EmptyPixel(current - 1, pathCopy) && !queue.includes(current - 1)) queue.push(current - 1);
-  //       if (current < 1056 && EmptyPixel(current + 33, pathCopy) && !queue.includes(current + 33)) queue.push(current + 33);
-  //       if (current % 33 < 32 && EmptyPixel(current + 1, pathCopy) && !queue.includes(current + 1)) queue.push(current + 1);
-  //       if (current > 32 && EmptyPixel(current - 33, pathCopy) && !queue.includes(current - 33)) queue.push(current - 33);
-  //     }
-  //   }
-  //   setCurr("");
-  // }
