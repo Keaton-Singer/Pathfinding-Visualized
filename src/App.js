22236f8a3faed80248096a721f87c84a437951f2
Keaton Singer <keatonsinger@Keatons-MacBook-Pro-2.local>
2023-01-09 17:18:45 -0800

    Working on Dropdown Menu

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
   const queueBFS = useRef([]);
   const [bitmap, setBitmap] = useState([]);
   const [algorithm, setAlgorithm] = useState("DFS");
-  const [algOpt, setAlgOpt] = useState("none");
-  const [speedOpt, setSpeedOpt] = useState("none");
-  const [toolsOpt, setToolsOpt] = useState("none");
-  const [gridOpt, setGridOpt] = useState("none");
+  const [algOption, setAlgOption] = useState("hidden");
+  const [speedOption, setSpeedOption] = useState("hidden");
+  const [toolsOption, setToolsOption] = useState("hidden");
+  const [bitmapOption, setBitmapOption] = useState("hidden");
   const [startButton, setStartButton] = useState("Start!");
   const startPersist = useRef("Start!");
-  const [origin, setOrigin] = useState(288);
-  const [target, setTarget] = useState(800);
+  const [origin, setOrigin] = useState(272);
+  const [target, setTarget] = useState(816);
   const [walls, setWalls] = useState([]);
   const [pathHead, setPathHead] = useState(-1);
   const path = useRef([]);
@@ -70,8 +70,9 @@ function App() {
     function stackPush(item) { stackDFS.current = [...stackDFS.current, item]; };
     if (stackDFS.current.length === 0) { stackPush(currentBit); };
     let topItem = -1;
-    while (stackDFS.current.length && startPersist.current === "Stop!") {
+    while (stackDFS.current.length) {
       await sleep(speed.current);
+      if (startPersist.current === "Start!") { break; }
       let lastIndex = stackDFS.current.length - 1;
       topItem = stackDFS.current[lastIndex];
       stackDFS.current = stackDFS.current.slice(0, lastIndex);
@@ -93,22 +94,20 @@ function App() {
     function queueAppend(item) { queueBFS.current = [...queueBFS.current, item]; };
     if (queueBFS.current.length === 0) { queueAppend(currentBit); };
     let nextItem = -1;
-    while (queueBFS.length !== 0 && startPersist.current === "Stop!") {
-      let queueLength = queueBFS.current.length;
-      for (let queueIndex = 0; queueIndex < queueLength; queueIndex++) {
-        await sleep(speed.current)
-        let lastIndex = queueBFS.current.length - 1;
-        nextItem = queueBFS.current[0];
-        queueBFS.current = queueBFS.current.slice(1, lastIndex);
-        if (walls.includes(nextItem)) { continue; }
-        setPathHead(nextItem);
-        if (nextItem === target) { queueBFS.current = []; break; }
-        path.current = [...path.current, nextItem];
-        if (nextItem > 32 && EmptyBit(nextItem - 33) && !queueBFS.current.includes(nextItem - 33)) queueAppend(nextItem - 33);
-        if (nextItem % 33 > 0 && EmptyBit(nextItem - 1) && !queueBFS.current.includes(nextItem - 1)) queueAppend(nextItem - 1);
-        if (nextItem < 1056 && EmptyBit(nextItem + 33) && !queueBFS.current.includes(nextItem + 33)) queueAppend(nextItem + 33);
-        if (nextItem % 33 < 32 && EmptyBit(nextItem + 1) && !queueBFS.current.includes(nextItem + 1)) queueAppend(nextItem + 1);
-      }
+    while (queueBFS.length !== 0) {
+      await sleep(speed.current)
+      if (startPersist.current === "Start!") { break; }
+      let lastIndex = queueBFS.current.length - 1;
+      nextItem = queueBFS.current[0];
+      queueBFS.current = queueBFS.current.slice(1, lastIndex + 1);
+      if (walls.includes(nextItem)) { continue; }
+      setPathHead(nextItem);
+      if (nextItem === target) { break; }
+      path.current = [...path.current, nextItem];
+      if (nextItem > 32 && EmptyBit(nextItem - 33) && !queueBFS.current.includes(nextItem - 33)) queueAppend(nextItem - 33);
+      if (nextItem % 33 < 32 && EmptyBit(nextItem + 1) && !queueBFS.current.includes(nextItem + 1)) queueAppend(nextItem + 1);
+      if (nextItem < 1056 && EmptyBit(nextItem + 33) && !queueBFS.current.includes(nextItem + 33)) queueAppend(nextItem + 33);
+      if (nextItem % 33 > 0 && EmptyBit(nextItem - 1) && !queueBFS.current.includes(nextItem - 1)) queueAppend(nextItem - 1);
     }
     if (path.current.indexOf(undefined) > -1 || path.current.length === 0) { path.current = []; return; }
     else if (nextItem === target || queueBFS.current.length === 0) { setStartButton("Clear Path!"); };
@@ -160,11 +159,11 @@ function App() {
 
 
   function ToggleCategory(state, setFunction) {
-    let categories = [setAlgOpt, setSpeedOpt, setToolsOpt, setGridOpt];
+    let categories = [setAlgOption, setSpeedOption, setToolsOption, setBitmapOption];
     for (let index = 0; index < 4; index++) {
-      categories[index]("none");
+      categories[index]("hidden");
     };
-    (state === "none") ? setFunction("block") : setFunction("none");
+    (state === "hidden") ? setFunction("visible") : setFunction("hidden");
   };
 
   return (
@@ -174,28 +173,79 @@ function App() {
           Pathfinding Visualized
         </div>
         <div id="Categories">
-          <div className="Category" onClick={() => {ToggleCategory(algOpt, setAlgOpt)}}>
+          <div className="Category" 
+            onClick={() => {ToggleCategory(algOption, setAlgOption)}}>
             Algorithms
-            <div onClick={() => { setAlgorithm("DFS"); CleanBitmap("partial"); }}>DFS</div>
-            <div onClick={() => { setAlgorithm("BFS"); CleanBitmap("partial"); }}>BFS</div>
+            <div className="Option" 
+              onClick={() => { setAlgorithm("DFS"); CleanBitmap("partial"); }}
+              style={{visibility: algOption}}>
+              DFS
+            </div>
+            <div className="Option" 
+              onClick={() => { setAlgorithm("BFS"); CleanBitmap("partial"); }}
+              style={{visibility: algOption}}>
+              BFS
+            </div>
+            <div className="Dummy"
+              style={{visibility: algOption}}>
+              .
+            </div>
           </div>
-          <div className="Category" onClick={() => {ToggleCategory(speedOpt, setSpeedOpt)}}>
+          <div className="Category" 
+            onClick={() => {ToggleCategory(speedOption, setSpeedOption)}}>
             Speed
-            <div onClick={() => { speed.current = 1000; }}>Slow</div>
-            <div onClick={() => { speed.current = 50; }}>Medium</div>
-            <div onClick={() => { speed.current = 1; }}>Fast</div>
+            <div className="Option" 
+              onClick={() => { speed.current = 1000; }}
+              style={{visibility: speedOption}}>
+              Slow
+            </div>
+            <div className="Option" 
+              onClick={() => { speed.current = 50; }}
+              style={{visibility: speedOption}}>
+              Medium
+            </div>
+            <div className="Option" 
+              onClick={() => { speed.current = 1; }}
+              style={{visibility: speedOption}}>
+              Fast
+            </div>
           </div>
-          <div className="Category" onClick={() => {ToggleCategory(toolsOpt, setToolsOpt)}}>
+          <div className="Category" onClick={() => {ToggleCategory(toolsOption, setToolsOption)}}>
             Tools
-            <div onClick={() => { setTool("Move Origin"); }}>Move Origin</div>
-            <div onClick={() => { setTool("Move Target"); }}>Move Target</div>
-            <div onClick={() => { setTool("Place Walls"); }}>Place Walls</div>
-            <div onClick={() => { setTool("Erase Walls"); }}>Erase Walls</div>
+            <div className="Option" 
+              onClick={() => { setTool("Move Origin"); }}
+              style={{visibility: toolsOption}}>
+              Move Origin
+            </div>
+            <div className="Option" 
+              onClick={() => { setTool("Move Target"); }}
+              style={{visibility: toolsOption}}>
+              Move Target
+            </div>
+            <div className="Option" 
+              onClick={() => { setTool("Place Walls"); }}
+              style={{visibility: toolsOption}}>
+              Place Walls
+            </div>
+            <div className="Option" 
+              onClick={() => { setTool("Erase Walls"); }}
+              style={{visibility: toolsOption}}>
+              Erase Walls
+            </div>
           </div>
-          <div className="Category" onClick={() => {ToggleCategory(gridOpt, setGridOpt)}}>
+          <div className="Category" 
+            onClick={() => {ToggleCategory(bitmapOption, setBitmapOption)}}>
             Bitmap
-            <div onClick={() => { CleanBitmap("partial"); }}>Clear Path</div>
-            <div onClick={() => { CleanBitmap("full"); }}>Reset Bitmap</div>
+            <div className="Option" 
+              onClick={() => { CleanBitmap("partial"); }}
+              style={{visibility: bitmapOption}}>
+              Clear Path
+            </div>
+            <div className="Option" 
+              onClick={() => { CleanBitmap("full"); }}
+              style={{visibility: bitmapOption}}>
+              Reset Bitmap
+            </div>
           </div>
         </div>
       </div>
